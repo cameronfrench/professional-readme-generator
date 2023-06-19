98af366bc47ffda644c4652e5c6a1fe5ea9a4bbf
Cameron French <cameron.french92@gmail.com>
2023-06-19 11:02:50 -0400

    added prompt questions for README

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,68 @@
 // TODO: Include packages needed for this application
 
+const inquirer = require('inquirer');
+const fs = require('fs');
+
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'project title',
+            message: 'Write a project title for your README',
+          },
+          {
+            type: 'input',
+            name: 'installation instructions',
+            message: 'Write your installation instructions for your application',
+          },
+          {
+            type: 'input',
+            name: 'usage information',
+            message: 'Write any usage information for your application',
+          },
+          {
+            type: 'input',
+            name: 'contribution guidelines',
+            message: 'Write any contribution guidelines for your application',
+          },
+          {
+            type: 'input',
+            name: 'test instructions',
+            message: 'Write any test instructions for your application',
+          },
+          {
+            type: "checkbox",
+            message: "Select a license for your application",
+            name: "license",
+            choices: [
+              { name: "MIT", value: "MIT" },
+              { name: "ISC", value: "ISC" },
+              { name: "Mozilla", value: "Mozilla" },
+              { name: "Apache", value: "Apache" },
+              { name: "EPL", value: "EPL" },
+              { name: "WTFPL", value: "WTFPL" },
+              { name: "None", value: "none" },
+            ],
+          },
+          {
+            type: "input",
+            message: "What is your github username?",
+            name: "gitName",
+          },
+          {
+            type: "input",
+            message: "What is your email address?",
+            name: "email",
+          },
+          {
+            type: "input",
+            message: "Instructions for how to reach you: ",
+            name: "prefCom",
+          },
+    ])
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
